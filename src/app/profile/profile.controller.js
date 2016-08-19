ab9a6a4d7874c3bc655a40e0a7206152d4902c7e
gnerkus <ifeanyioraelosi@gmail.com>
2016-08-19 14:49:23 +0100

    [Profile] define form for updating username on profile page

--- src/app/profile/profile.controller.js
+++ src/app/profile/profile.controller.js
@@ -1,1 +1,17 @@
-console.log('Placeholder');
+define([
+  'angular',
+], function (angular) {
+  'use strict';
+
+  return ['$scope', 'ProfileService', function ($scope, ProfileService) {
+    $scope.getName = function() {
+      $scope.username = ProfileService.getName();
+    };
+
+    $scope.updateUserame = function() {
+      ProfileService.setName($scope.username);
+    };
+
+    $scope.getName();
+  }];
+});
