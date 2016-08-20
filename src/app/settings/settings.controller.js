0e69ee54924b59990a3dd902a3fa057640fac6d7
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2016-08-20 20:02:03 +0100

    Feat settings list (#6)
    * [Core] install Lodash
    
    * [Settings] define Settings service
    
    The Settings service contains a list of 50 items that represent user
    'settings'. These settings are changed on the settings page.
    
    * [Settings] add list of settings to settings page
    
    This commit also defines a form to sort the settings by two properties
    and a form to search the settings.

--- src/app/settings/settings.controller.js
+++ src/app/settings/settings.controller.js
@@ -1,1 +1,28 @@
-console.log('Placeholder')
+define([
+  'angular',
+  'lodash'
+], function (angular, lodash) {
+  'use strict';
+
+  return ['$scope', 'SettingsService', 'ProfileService', function ($scope, SettingsService, ProfileService) {
+    console.info(ProfileService);
+    console.info(ProfileService.getName());
+    $scope.username = ProfileService.getName();
+    $scope.settings = SettingsService.getSettings();
+
+    // Sorting parameters
+    $scope.sortParams = {
+      category: false,
+      priority: false,
+    };
+
+    // Sorting function
+    $scope.sortSettings = function () {
+      // Fetch the sort params that have been set
+      var setParams = Object.keys($scope.sortParams).filter(function(key) {
+        return $scope.sortParams[key];
+      });
+      $scope.settings = lodash.sortBy($scope.settings, setParams);
+    };
+  }];
+});
