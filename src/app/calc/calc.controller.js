3dfb03cb9ffb24e94ab893391dcde7d4b8b802de
gnerkus <ifeanyioraelosi@gmail.com>
2016-08-21 20:46:06 +0100

    [Calc] define simple arithmetic calculator

--- src/app/calc/calc.controller.js
+++ src/app/calc/calc.controller.js
@@ -14,5 +14,22 @@ define([
 
     // Fetch the default username
     $scope.getName();
+
+    /* Input sum variables */
+    // A variable to hold the last sum computed
+    var currentSum = null;
+    $scope.computeSum = function() {
+      $scope.numberSum = $scope.firstNum + $scope.secondNum + $scope.thirdNum;
+      // Store the current sum
+      currentSum = $scope.numberSum;
+    };
+
+    $scope.computeQuotient = function() {
+      var perDiff = $scope.numberSum / currentSum;
+      $scope.firstNum *= perDiff;
+      $scope.secondNum *= perDiff;
+      $scope.thirdNum *= perDiff;
+      currentSum = $scope.numberSum;
+    }
   }];
 });
